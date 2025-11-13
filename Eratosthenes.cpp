4cdfe2009f88e3352bf8f74f9004165790413228
asmalljj <653595478@qq.com>
2025-11-13 10:44:19 +0800

    埃拉托色尼筛找素数

---
+++ Eratosthenes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+    // 输出所有素数
+    for (int i = 2; i <= n; ++i) {
+        if (is_prime[i]) cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
